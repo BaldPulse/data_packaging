bf76a4421b81020776b79bd9e83b04c9aeea494c
Zhao Tang <tangzhao1999@hotmail.com>
2024-09-10 19:05:21 +0800

    made code independent of container

--- docker_run.sh
+++ docker_run.sh
@@ -9,6 +9,8 @@ HOST_OUTPUT_FOLDER="path/to/output/folder" #TODO: Replace with the path to your 
 CONTAINER_OUTPUT_FOLDER="/output" # Folder inside the container
 HOST_OUTPUT_FOLDER="path/to/buffer/folder" #TODO: Replace with the path to your host buffer folder
 CONTAINER_OUTPUT_FOLDER="/buffer" # Folder inside the container
+HOST_REPO_FOLDER="path/to/this/repo" #TODO: Replace with the path to this repo
+CONTAINER_REPO_FOLDER="/data_packaging" # Folder inside the container
 NUM_THREADS="2" #TODO: Number of threads used by the data packager
 
 # Start the Docker container in the background with volume mapping
@@ -17,6 +19,7 @@ docker run -it --rm \
     -v $HOST_DATA_FOLDER:$CONTAINER_DATA_FOLDER \
     -v $HOST_OUTPUT_FOLDER:$CONTAINER_OUTPUT_FOLDER\
     -v $HOST_BUFFER_FOLDER:$CONTAINER_BUFFER_FOLDER\
+    -v $HOST_REPO_FOLDER:$CONTAINER_REPO_FOLDER\
     $IMAGE_NAME \
     python3 /data_packaging/continuous_process.py -d /data -o /output -b /buffer -n $NUM_THREADS
 
