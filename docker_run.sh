5b3f2cf9f68a15117cecab16fc478e35b4bb00ce
Zhao Tang <tangzhao1999@hotmail.com>
2024-09-10 17:59:44 +0800

    docker run minor change

--- docker_run.sh
+++ docker_run.sh
@@ -12,16 +12,12 @@ CONTAINER_OUTPUT_FOLDER="/buffer" # Folder inside the container
 NUM_THREADS="2" #TODO: Number of threads used by the data packager
 
 # Start the Docker container in the background with volume mapping
-docker run -it \
+docker run -it --rm \
     --name $CONTAINER_NAME \
     -v $HOST_DATA_FOLDER:$CONTAINER_DATA_FOLDER \
     -v $HOST_OUTPUT_FOLDER:$CONTAINER_OUTPUT_FOLDER\
     -v $HOST_BUFFER_FOLDER:$CONTAINER_BUFFER_FOLDER\
     $IMAGE_NAME \
     python3 /data_packaging/continuous_process.py -d /data -o /output -b /buffer -n $NUM_THREADS
-
-# Optionally, stop and remove the container
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
 
 echo "Command executed and container cleaned up."
